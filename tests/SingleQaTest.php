16fe0c09121ce63c2eda8fe60091ce11d8738932
Mark Jordan <mjordan@sfu.ca>
2017-02-08 19:58:32 -0800

    Fixed tests.

--- tests/SingleQaTest.php
+++ tests/SingleQaTest.php
@@ -13,20 +13,20 @@ class SingleQaTest extends \PHPUnit_Framework_TestCase
 
     public function testRunSuccessfulQA()
     {
-        $single = new \iipqa\contentmodels\Single($this->inputDirectoryPass, $this->pathToLog);
+        $single = new \iipqa\contentmodels\Single($this->inputDirectoryPass, $this->pathToLog, null);
         $this->assertContains(true, $single->applyQaTests());
     }
 
     public function testCheckExtensions()
     {
-        $single = new \iipqa\contentmodels\Single($this->inputDirectoryFail, $this->pathToLog);
+        $single = new \iipqa\contentmodels\Single($this->inputDirectoryFail, $this->pathToLog, null);
         $single->applyQaTests();
         $this->assertContains("5 => 'png'", file_get_contents($this->pathToLog));
     }
 
     public function testCheckFilePairs()
     {
-        $single = new \iipqa\contentmodels\Single($this->inputDirectoryFail, $this->pathToLog);
+        $single = new \iipqa\contentmodels\Single($this->inputDirectoryFail, $this->pathToLog, null);
         $single->applyQaTests();
         $this->assertContains("2 => 2", file_get_contents($this->pathToLog));
         $this->assertContains("3 => 4", file_get_contents($this->pathToLog));
@@ -34,7 +34,7 @@ class SingleQaTest extends \PHPUnit_Framework_TestCase
 
     public function testCheckForDirectories()
     {
-        $single = new \iipqa\contentmodels\Single($this->inputDirectoryFail, $this->pathToLog);
+        $single = new \iipqa\contentmodels\Single($this->inputDirectoryFail, $this->pathToLog, null);
         $single->applyQaTests();
         $this->assertContains("tests/fixtures/single/fail/foo", file_get_contents($this->pathToLog));
     }
